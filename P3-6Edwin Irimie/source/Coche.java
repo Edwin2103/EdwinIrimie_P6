2b518a60e1359b8fdd1ecb236dd0d0ec7f1a7c63
Edwin2103 <32304460+Edwin2103@users.noreply.github.com>
2018-01-28 20:03:56 +0100

    Add files via upload

--- P3-6Edwin Irimie/source/Coche.java
+++ P3-6Edwin Irimie/source/Coche.java
@@ -1,6 +1,12 @@
 import javax.swing.JOptionPane;
 
-class Coche {
+/**
+ * Una clase para representar los coches con sus caracteristicas y metodos ademas de constructores.
+ * @version, 28/01/18
+ * @author Edwin Orlando Irimie
+ */
+
+public class Coche {
 	
 	private String matricula;
 	private String marca;
@@ -10,7 +16,22 @@ class Coche {
 	private double km;
 	private int npuertas;
 	private int nplazas;
+	public static int numcoche;
+	static final int MAX_COCHES = 5;
 	
+	/** 
+     * Crea un coche a partir de las siguientes variable
+     * @param matricula que se utiliza para guardar la matricula 
+     * @param marca que da valor a la marca del nuevo coche 
+     * @param color que guarda un color que le asignaremos 
+     * @param modelo guradara el modelo del coche 
+     * @param  techo es una variable que devolvera un si o no segun si tiene o no techo
+     * @param  km variable de tipo int que guardara el numero de kilometros
+     * @param  npuertas variable de tipo int que guardara el numero de puertas
+     * @param  nplazas variable de tipo int que guardara el numero de plazas
+     * @param  de tipo int y estatico que realiza la funcion de contador
+     * @param  de tipo int se utiliza para que el programa no sobrepase los 5 coches
+     */
 	
 	void setMatricula(String matri){
 		matricula = matri;
@@ -18,30 +39,66 @@ class Coche {
 	String getMatricula(){
 		return matricula;
 	}
+	
+	/**
+	 *en el metodo setMatricula le pasamos por parametro la variable matri
+	 *igualamos esa variable a la de matricula
+	 *en el metodo getMatricula
+	 *@return matricula
+	 */
+	 
 	void setMarca(String marc){
 		marca = marc;
 	}
 	String getMarca(){
 		return marca;
 	}
+	
+	/**
+	 *en el metodo setMarca le pasamos por parametro la variable marc
+	 *igualamos esa variable a la de marca
+	 *@return marca
+	 */
+	 
 	void setModelo(String model){
 		modelo = model;
 	}
 	String getModelo(){
 		return modelo;
 	}
+	
+	/**
+	 *en el metodo setModelo le pasamos por parametro la variable model
+	 *igualamos esa variable a la de modelo
+	 *@return modelo
+	 */
+	 
 	void setColor(String col){
 		color = col;
 	}
 	String getColor(){
 		return color;
 	}
+	
+	/**
+	 *en el metodo setColor le pasamos por parametro la variable col
+	 *igualamos esa variable a la de color
+	 *@return color
+	 */
+	 
 	void setTecho(String tech){
 		techo = tech;
 	}
 	String getTecho(){
 		return techo;
 	}
+	
+	/**
+	 *en el metodo setTecho le pasamos por parametro la variable tch
+	 *igualamos esa variable a la de techo
+	 *@return techo
+	 */
+	 
 	void setKm(double kim){
 		if ( kim > 0) {	
 			km = kim;
@@ -53,6 +110,13 @@ class Coche {
 	double getKm(){
 		return km;
 	}
+	
+	/**
+	 *se le pasa por parametro kim y mediante la condicion del if se determina
+	 *si el coche tiene mas de 0 se igual kim con km y si no los km valen 0
+	 *@return km
+	 */
+	 
 	void setNpuertas(int npuer){
 		if ( npuer > 0 && npuer <= 5){
 			npuertas = npuer;
@@ -64,6 +128,13 @@ class Coche {
 	int getNpuertas(){
 		return npuertas;
 	}
+	
+	/**
+	 *se le pasa por parametro npuer y mediante la condicion del if se determina
+	 *si el coche tiene 5 o menos puertas
+	 *@return npuertas
+	 */
+	 
 	void setNplazas(int nplaz){
 		if ( nplaz > 0 && nplaz <= 7){
 			nplazas = nplaz;
@@ -76,27 +147,62 @@ class Coche {
 		return nplazas;
 	}
 	
+	/**
+	 *se le pasa por parametro nplaz y mediante la condicion del if se determina
+	 *si el coche tiene 7 o mas plazas
+
+	 */
+	 
+	
 	public Coche(){
 		matricula = " ";
 		marca = "Seat";
 		modelo = "Altea";
-		color = "blanco";
+		color = "Blanco";
 		techo = "No";
 		km = 0;
 		npuertas = 3;
 		nplazas = 5;
+		numcoche++;
 	}
+	
+	/*
+	 *constructor Coche sin parametros
+	 *@param matricula = le asignamos una matricula
+	 *@param marca = le asignamos una marca
+	 *@param modelo = le asignamos un modelo
+	 *@param color = le asignamos un color
+	 *@param techo = le asignamos si tiene techo o no
+	 *@param km = le asignamos unos kilometros
+	 *@param npuertas = asignamos un numero de puertas
+	 *@param nplazas = asignamos un numero de plazas 
+	 *@param numcoche el contador se incrementa
+	 */
 		
 	public Coche(String cochematricula){
 		matricula = cochematricula;
 		marca = "Seat";
 		modelo = "Altea";
-		color = "blanco";
+		color = "Blanco";
 		techo = "No";
 		km = 0;
 		npuertas = 3;
 		nplazas = 5;
+		numcoche++;
 	}
+	
+	/*
+	 *constructor Coche con un parametro
+	 *@param cochematricula = le asignamos una matricula
+	 *@param marca = le asignamos el valor por defecto
+	 *@param modelo = le asignamos el valor por defecto
+	 *@param color = le asignamos el valor por defecto
+	 *@param techo = le asignamos el valor por defecto
+	 *@param km = le asignamos el valor por defecto
+	 *@param npuertas = le asignamos el valor por defecto
+	 *@param nplazas = le asignamos el valor por defecto
+	 *@param numcoche el contador se incrementa
+	 */
 		
 	public Coche(int numPuertas , int numPlazas){
 		npuertas = numPuertas;
@@ -104,10 +210,24 @@ class Coche {
 		matricula = " ";
 		marca = "Seat";
 		modelo = "Altea";
-		color = "blanco";
+		color = "Blanco";
 		techo = "No";
 		km = 0;
+		numcoche++;
 	}
+	
+	/*
+	 *constructor Coche con dos parametros
+	 *@param matricula = le asignamos el valor por defecto
+	 *@param marca = le asignamos el valor por defecto
+	 *@param modelo = le asignamos el valor por defecto
+	 *@param color = le asignamos el valor por defecto
+	 *@param techo = le asignamos el valor por defecto
+	 *@param km = le asignamos el valor por defecto
+	 *@param numPuertas = asignamos un numero de puertas 
+	 *@param numPlazas = asignamos un numero de plazas 
+	 *@param numcoche el contador se incrementa
+	 */
 		
 	public Coche(String marCoche , String moCoche , String coCoche){
 		marca = marCoche;
@@ -118,25 +238,64 @@ class Coche {
 		km = 0;
 		npuertas = 3;
 		nplazas = 5;
+		numcoche++;
 	}
+	
+	/*
+	 *constructor Coche con tres parametros
+	 *@param marCoche se le asigna una matricula diferente a la prederteminada
+	 *@param moCoche se le asigna un modelo diferente a la prederteminada
+	 *@param coCoche se le asigna un color diferente a la prederteminada
+	 *@param techo = le asignamos el valor por defecto
+	 *@param km = le asignamos el valor por defecto
+	 *@param npuertas = le asignamos el valor por defecto
+	 *@param nplazas = le asignamos el valor por defecto
+	 *@param numcoche el contador se incrementa
+	 */
 	
 	void avanzar(double kilom){
 		km = km + kilom;
-		JOptionPane.showMessageDialog(null,"El coche ha avanzado " + kilom + "km y tiene " + km + " km ");
+		
 	}
+	
+	/* mediante el metodo avanzar calculamos los kilometros que se suman a los que ya tenia
+	 *@param kilom hace referencia a los kilometros resltantes
+	*/
+	
 	void tunear(){
 		km = 0;
-		techo = "Si";
-		JOptionPane.showMessageDialog(null,"El coche dispone de techo solar: " + techo );
+		if (techo.equals("No")){
+			techo = "Si";
+		}
+		else{
+			techo = "No";
+		} 
 	}
+	
+	/* 
+	 *comprobamos si ya tiene techo y si es asi lo cambia a que no y viceversa
+	 *ademas cambia los kilometros a 0
+	 */
+	 
 	void tunear(String colo){
 		color = colo;
-		techo = "Si";
-		JOptionPane.showMessageDialog(null,"El coche dispone de techo solar: " + techo + " y tiene color " + colo);
+		
 	}
+	
+	/*
+	 *@param colo 
+	 *le asignamos el color
+	 */
+	 
+	 
 	void matricular(String matricu){
 		matricula = matricu;
 	}
+	
+	/*
+	 *@param matricu 
+	 *le asignamos la matricula
+	 */
 }
 		
 		
--- P3-6Edwin Irimie/source/Fabrica.java
+++ P3-6Edwin Irimie/source/Fabrica.java
@@ -2,56 +2,209 @@ import javax.swing.JOptionPane;
 
 public class Fabrica{
 	
+	/*
+	 *@param cars que es una array
+	 *@param num que se inizializa a 0
+	 *@param matric que hace referencia a la nueva matricula que se le introducira
+	 *@param coch hace referencia a la matricula guardada despues de pasarla a int
+	 */
+	
 
 	public static void main(String[] args){
 		
-		String coche = "Este programa esta desarrollado por Edwin Irimie";
-		JOptionPane.showMessageDialog(null,coche);
+		String coche = "Este programa esta desarrollado por Edwin Irimie";	
 		
-		Coche [] cars = new Coche[4];
+		Coche [] cars = new Coche[Coche.MAX_COCHES];
 		
-		Coche car1 = new Coche();
-		Coche car2 = new Coche("5678-AG");
-		Coche car3 = new Coche(5 , 5);
-		Coche car4 = new Coche("VW" , "Caddy" , "blanco");
+		int num = 0;
+		String matric;
+		int coch;
 		
-		car1.matricular("1234-DF");
-		car1.setModelo("Toledo");
-		car1.setColor("azul");
-		car1.avanzar(200);
 		
-		car2.setMarca("Fiat");
-		car2.setModelo("Uno");
-		car2.tunear("rojo");
-		car2.avanzar(300);
-		car2.setNplazas(2);
+		do{
+			try{
+				
+				num = Integer.parseInt(JOptionPane.showInputDialog(null, "Fabrica de Coches de Edwin. Selecciona una de las siguientes opciones: \n" 
+				+ "1.Fabricar coche(conociendo matricula) \n" + "2.Fabricar coche(a partir del numero de puertas y el numero de plazas) \n" + 
+				"3.Fabricar coche(a partir de la marca, el modelo y el color) \n" + "4.Fabricar coche(sin datos) \n" + "5.Tunear coche(pintandolo solo) \n" + 
+				"6.Tunear coche(sin pintarlo) \n" + "7.Avanzar kilometros \n" + "8.Mostrar caracteristicas de un coche \n" + "9.Salir del programa \n"));
+				switch (num) {
+					case 0: 
+						JOptionPane.showMessageDialog(null,"La opcion introducida no se encuentra");
+					break;
+					case 1: 
+						if(Coche.numcoche != 5){
+							String matricula = JOptionPane.showInputDialog(null,"Inserte una matricula: ");
+							cars[Coche.numcoche] = new Coche(matricula);
+							caracteristicas(cars[Coche.numcoche -1]);
+							
+						}
+						else{
+							JOptionPane.showMessageDialog(null,"No puede crear mas de 5 coches");
+						}
+					break;
+					case 2:
+						if(Coche.numcoche != 5){
+							String puertas = JOptionPane.showInputDialog(null,"Inserte numero de puertas: ");
+							String plazas = JOptionPane.showInputDialog(null,"Inserte numero de plazas: ");
+							int puer = Integer.parseInt(puertas);
+							int plaz = Integer.parseInt(plazas);
+							cars[Coche.numcoche] = new Coche(puer , plaz);
+							matAleatorio(cars[Coche.numcoche -1]);
+							caracteristicas(cars[Coche.numcoche -1]);
+							
+						}
+						else{
+							JOptionPane.showMessageDialog(null,"No puede crear mas de 5 coches");
+						}
+					break;
+					case 3:
+						if(Coche.numcoche != 5){
+							String marca = JOptionPane.showInputDialog(null,"Inserte marca: ");
+							String modelo = JOptionPane.showInputDialog(null,"Inserte modelo: ");
+							String color = JOptionPane.showInputDialog(null,"Inserte color: ");
+							cars[Coche.numcoche] = new Coche(marca , modelo , color);
+							matAleatorio(cars[Coche.numcoche -1]);
+							caracteristicas(cars[Coche.numcoche -1]);
+						}
+						else{
+							JOptionPane.showMessageDialog(null,"No puede crear mas de 5 coches");
+						}	
+					break;
+					case 4:
+						if(Coche.numcoche != 5){
+							cars[Coche.numcoche] = new Coche();
+							matAleatorio(cars[Coche.numcoche -1]);
+							caracteristicas(cars[Coche.numcoche -1]);
+						}
+						else{
+							JOptionPane.showMessageDialog(null,"No puede crear mas de 5 coches");
+						}		
+					break;
+					case 5:
+							
+							matric = JOptionPane.showInputDialog(null,"Inserte la matricula ");
+							coch = buscaCoche(matric, cars);
+							
+							if(coch != -1){
+								String color = JOptionPane.showInputDialog(null,"Inserte el color:");
+								cars[coch].tunear(color);
+								caracteristicas(cars[coch]);
+							}
+							else{
+								JOptionPane.showMessageDialog(null,"No hay ningun coche con esa matricula");
+							}				
+					break;
+					case 6:
+						
+							matric = JOptionPane.showInputDialog(null,"Inserte la matricula ");
+							coch = buscaCoche(matric, cars);
+							
+							if(coch != -1){
+							
+								cars[Coche.numcoche -1].tunear();
+								caracteristicas(cars[coch]);
+							}
+							else{
+								JOptionPane.showMessageDialog(null,"No hay ningun coche con esa matricula");
+							}	
+							
+					break;
+					case 7:
+							
+							matric = JOptionPane.showInputDialog(null,"Inserte la matricula ");
+							coch = buscaCoche(matric, cars);
+							
+							if(coch != -1){
+							
+								String km = JOptionPane.showInputDialog(null,"Inserte km: ");
+								double x = Double.parseDouble(km);
+								cars[coch].avanzar(x);
+								JOptionPane.showMessageDialog(null,"El coche cuya matricula es: " + matric + " ha avanzado " + x + "km y tiene " + x + " km ");
+								caracteristicas(cars[coch]);
+							}
+							else{
+								JOptionPane.showMessageDialog(null,"No hay ningun coche con esa matricula");
+							}	
+							
+					break;
+					case 8:
+						
+							matric = JOptionPane.showInputDialog(null,"Inserte la matricula ");
+							coch = buscaCoche(matric, cars);
+							
+							if(coch != -1){
+							
+								caracteristicas(cars[coch]);
+							}
+							else{
+								JOptionPane.showMessageDialog(null,"No hay ningun coche con esa matricula");
+							}	
+							
+					break;
+					case 9:	
+						JOptionPane.showMessageDialog(null,"Gracias por utilizar este programa");
+					break;
+					default: 
+						JOptionPane.showMessageDialog(null,"La opcion introducida no es correcta");
+					break;
+				}
+			}
+			catch(Exception e){
+				JOptionPane.showMessageDialog(null,"La opcion introducida no es correcta");
+			}
+		}while(num != 9);
+	}
+	
+	/* 
+	 *dentro del metodo main se realiza un do while para que vaya repitiendo siempre que la opcion no sea 9
+	 *dentro del do while se hace un try catch para comprobar que la opcion sea numerica 
+	 *y por ultimo dentro del try se realiza el switch con las opciones
+	 */
+	 
+	public static void caracteristicas(Coche c){	
+	
 		
-		car3.matricular("9012-HH");
-		car3.setMarca("BMW");
-		car3.setModelo("850");
-		car3.setColor("gris");
-		car3.avanzar(400);
-		car3.setNpuertas(5);
-		
-		car4.matricular("3456-XS");
-		car4.tunear();
-		car4.avanzar(500);
-		car4.setNpuertas(5);
-		car4.setNplazas(7);
-		
-		cars[0] = car1;
-		cars[1] = car2;
-		cars[2] = car3;
-		cars[3] = car4;
-		
-		for ( int i = 0; i < cars.length; i++){
-			caracteristicas(cars[i]);
-		}
-		
-	}	
-	static void caracteristicas(Coche c){	
-			
-		JOptionPane.showMessageDialog(null,"La matricula es " + c.getMatricula() + "\n la marca del coche es " + c.getMarca() + "\n el modelo es " + c.getModelo() + "\n el color es " + c.getColor() + "\n techo solar " + c.getTecho() + "\n tiene " + c.getKm() + " km " + "\n numero de puertas " + c.getNpuertas() + "\n numero de plazas " + c.getNplazas());	
+		JOptionPane.showMessageDialog(null,"El coche tiene las siguientes caracteristicas : " + "\n Matricula =  " + c.getMatricula() + "\n Marca =  " + c.getMarca() + "\n Modelo =  " + c.getModelo() + "\n Color = " + c.getColor() + "\n Techo solar =  " + c.getTecho() + "\n Kilometros =  " + c.getKm() + " km " + "\n Numero de puertas =  " + c.getNpuertas() + "\n Numero de plazas = " + c.getNplazas());	
+		JOptionPane.showMessageDialog(null,"Numero de coches creados: " + c.numcoche);
+	}
+	
+	/*
+	 *mostraralas caracteristicas de cada coche
+	 *@param c que es de tipo Coche
+	 */
+	 
+
+	public static void matAleatorio(Coche co){
+		String mate = Integer.toString((int) (Math.random() * 100000));
+		co.setMatricula(mate);
 		
 	}
+	
+	/*
+	 *genera una matricula aleatoria
+	 *@param co que es de tipo Coche
+	 */
+	 
+	public static int buscaCoche( String buscmatricula, Coche[] cars){
+		
+		int x = -1;
+		for (int i = 0; i < Coche.numcoche; i++){
+			
+			String matri = cars[i].getMatricula();
+			if(matri.equals(buscmatricula)){
+				x = i;
+				return x;
+			}
+		}
+		return x;
+	}
+	
+	/*
+	 *buscara si la matricula introducida existe si es el caso la devuelve
+	 *@return x devuelve el valor de la matricula
+	 *@param buscmatricula guada la matricula
+	 *@param cars es la array y proporciona la posicion de dicha matriculaç
+	 */
+	
 }
